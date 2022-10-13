9f41171edb42db2e191a7e0c84ce0788bbec2763
leonardiodecaprio <dafathni@gmail.com>
2022-10-13 14:37:22 -0700

    try try try

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,27 @@
+nes (11 sloc)  184 Bytes
+
+
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *   * main - Prints a text
+ *
+ *     *
+ *
+ *       * Return: Always 0 (Success)
+ *
+ *         */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
