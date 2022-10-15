182cf6a2ea1c4d699e27c874f5193c97cf0bf6a8
leonardiodecaprio <dafathni@gmail.com>
2022-10-15 01:32:09 -0700

    ouful

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,4 @@
 #include<stdio.h>
-
-
-
 /**
  *
  * * main - Entry point
@@ -10,15 +7,9 @@
  *
  * * Return: Always 0 (Success)
  *
- */ 
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-
-
